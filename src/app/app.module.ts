bb7a615260cfa729dafce5a347984ad49b3de857
陈孔超 <ckc1022@163.com>
2019-01-19 11:04:04 +0800

    测试-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { ChenComponent } from './test/chen/chen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    ChenComponent
   ],
   imports: [
     BrowserModule,
